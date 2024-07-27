68076701296de89d3699ecf8d5053e0788a7fffa
BinayakDeveloper <banjarajajabar@gmail.com>
2024-07-28 00:27:04 +0530

    Fixed Major Bugs

--- script/script.js
+++ script/script.js
@@ -1,3 +1,126 @@
+// Display Alarms Function
+function displayAlarms(alarmContainer, alarmsData) {
+  let alarms = JSON.parse(alarmsData);
+  let uniqueAlarms = [];
+  let seenIds = [];
+
+  alarms.forEach((alarm) => {
+    if (!seenIds.includes(alarm.alarmId)) {
+      uniqueAlarms.push(alarm);
+      seenIds.push(alarm.alarmId);
+    }
+  });
+
+  localStorage.setItem("alarms", JSON.stringify(uniqueAlarms));
+  let alarmHTML = "";
+
+  uniqueAlarms.forEach(
+    ({ alarmId, alarmTime, alarmExt, alarmStatus, alarmNote }) => {
+      alarmHTML += `<div class="alarm" data-alarm-id=${alarmId}>
+          <div class="leftContent">
+            <div class="leftTop">
+              <h1>${
+                alarmTime.split(":")[0] > 12
+                  ? alarmTime.split(":")[0] - 12
+                  : alarmTime.split(":")[0]
+              }:${alarmTime.split(":")[1]}</h1>
+              <h5>${alarmExt}</h5>
+            </div>
+            <div class="leftBottom">
+              <p>Ring once</p>
+              ${alarmNote !== "" ? `<p>| ${alarmNote}</p>` : ""}
+            </div>
+          </div>
+          <input class="checkbox" type="checkbox" name="onOffData" id="onOffData${alarmId}" ${
+        alarmStatus ? "checked" : ""
+      } />
+          <label for="onOffData${alarmId}"
+            ><div class="rightContent">
+              <div class="button ${
+                alarmStatus ? "buttonActive" : ""
+              }" data-alarm-id=${alarmId}>
+                <div class="buttonDot ${alarmStatus ? "dotActive" : ""}"></div>
+              </div>
+            </div>
+          </label>
+        </div>`;
+    }
+  );
+  alarmContainer.innerHTML = alarmHTML;
+
+  // Update Alarm Info
+  let allAlarms = document.querySelectorAll(".alarm");
+
+  allAlarms.forEach((alarm, ind) => {
+    alarm.addEventListener("click", (e) => {
+      let targetClassList = Array.from(e.target.classList);
+
+      let isButton =
+        targetClassList.includes("buttonDot") ||
+        targetClassList.includes("button") ||
+        targetClassList.includes("checkbox");
+
+      if (!isButton) {
+        timeInput.value = alarms[ind].alarmTime;
+        alarmNoteInput.value = alarms[ind].alarmNote;
+        selectedSong.textContent = alarms[ind].ringtoneName;
+        alarms[ind].alarmNote !== ""
+          ? resetBtn.classList.add("resetShow")
+          : resetBtn.classList.remove("resetShow");
+        ringtones.forEach((ringtone) => {
+          if (
+            ringtone.getAttribute("data-song-name").toLowerCase() ===
+            alarms[ind].ringtoneName.toLowerCase()
+          ) {
+            ringtone.checked = true;
+          }
+        });
+        alarmEventContainer.setAttribute("data-edit-alarm-id", ind + 1);
+        alarmEventContainer.classList.add("showAlarmEventContainer");
+      }
+    });
+  });
+
+  // Alarm On/Off Updation
+  let buttons = document.querySelectorAll(".button");
+  let buttonDots = document.querySelectorAll(".buttonDot");
+  let checkBoxes = document.querySelectorAll("input[type='checkbox']");
+
+  buttons.forEach((val, ind) => {
+    val.addEventListener("click", () => {
+      alarmEventContainer.classList.remove("showAlarmEventContainer");
+      buttons[ind].classList.toggle("buttonActive");
+      buttonDots[ind].classList.toggle("dotActive");
+      let buttonStatus = !checkBoxes[ind].checked;
+      let alarmId = val.getAttribute("data-alarm-id");
+      let updatedAlarms = [];
+      let alarms = JSON.parse(localStorage.getItem("alarms"));
+      alarms.forEach((alarm) => {
+        if (alarm.alarmId == alarmId) {
+          alarm.alarmStatus = buttonStatus;
+        }
+        updatedAlarms.push(alarm);
+      });
+      localStorage.setItem("alarms", JSON.stringify(updatedAlarms));
+    });
+  });
+}
+
+// Alarm Active
+var alarmInterval;
+function alarmActive() {
+  clearInterval(alarmInterval);
+  alarmInterval = setInterval(() => {
+    let alarmsData = localStorage.getItem("alarms");
+    if (alarmsData) {
+      let onAlarms = JSON.parse(alarmsData).filter((alarm) => {
+        return alarm.alarmStatus === true;
+      });
+      console.log(onAlarms);
+    }
+  }, 700);
+}
+
 // Pause Remaining Audios
 function pausePrevAudio() {
   if (ringtoneAudio !== undefined) {
@@ -22,6 +145,10 @@ addBtn.addEventListener("click", () => {
 let cancelBtn = document.querySelector(".eventCancel");
 cancelBtn.addEventListener("click", () => {
   pausePrevAudio();
+  alarmEventContainer.setAttribute("data-edit-alarm-id", "");
+  alarmNoteInput.value = "";
+  ringtones[0].checked = true;
+  selectedSong.textContent = ringtones[0].getAttribute("data-song-name");
   alarmEventContainer.classList.remove("showAlarmEventContainer");
 });
 
@@ -58,7 +185,7 @@ let ringtones = document.querySelectorAll(".songRadio");
 ringtones.forEach((song) => {
   song.addEventListener("click", () => {
     selectedSong.textContent = song.getAttribute("data-song-name");
-    if (ringtoneAudio == undefined) {
+    if (ringtoneAudio === undefined) {
       ringtoneAudio = new Audio(song.value);
       ringtoneAudio.loop = true;
       ringtoneAudio.play();
@@ -75,103 +202,224 @@ ringtones.forEach((song) => {
 
 // Display Alarms
 let alarmContainer = document.querySelector(".alarmContainer");
-document.addEventListener("DOMContentLoaded", () => {
-  let json = [
-    {
-      alarmId: 1,
-      alarmTime: "10:02",
-      alarmExt: "PM",
-      alarmNote: "",
-      alarmStatus: false,
-      ringtoneName: "Casimiro",
-      ringtonePath: "./Assets/Ringtones/casimiro.mp3",
-    },
-    {
-      alarmId: 2,
-      alarmTime: "10:25",
-      alarmExt: "AM",
-      alarmNote: "Hello",
-      alarmStatus: true,
-      ringtoneName: "Odacity",
-      ringtonePath: "./Assets/Ringtones/odacity.mp3",
-    },
-  ];
-
-  localStorage.setItem("alarms", JSON.stringify(json));
+window.addEventListener("DOMContentLoaded", () => {
   let alarmsData = localStorage.getItem("alarms");
 
-  if (alarmsData === null) {
+  // Remove Expired Alarm
+
+  setTimeout(() => {
+    let currentUTCmilliseconds = Date.now();
+    let updatedAlarms = [];
+    if (alarmsData) {
+      JSON.parse(alarmsData).forEach((alarm) => {
+        let alarmYear = new Date(alarm.alarmSetTime).getFullYear();
+        let alarmMonth = new Date(alarm.alarmSetTime).getMonth();
+        let alarmDate = new Date(alarm.alarmSetTime).getDate();
+        let alarmHour = alarm.alarmTime.split(":")[0];
+        let alarmMins = alarm.alarmTime.split(":")[1];
+
+        let requiredMS = new Date(
+          alarmYear,
+          alarmMonth,
+          alarmDate,
+          alarmHour,
+          alarmMins
+        ).getTime();
+
+        if (requiredMS > currentUTCmilliseconds) {
+          updatedAlarms.push(alarm);
+        }
+      });
+    }
+
+    if (updatedAlarms.length !== 0) {
+      localStorage.setItem("alarms", JSON.stringify(updatedAlarms));
+    } else {
+      localStorage.removeItem("alarms");
+    }
+  });
+
+  alarmsData = localStorage.getItem("alarms");
+
+  alarmActive();
+
+  if (alarmsData == null) {
     let noAlarmContainer = document.querySelector(".noAlarm");
     noAlarmContainer.classList.add("noAlarmShow");
   } else {
-    let alarms = JSON.parse(alarmsData);
-    alarms.forEach(
-      ({ alarmId, alarmTime, alarmExt, alarmStatus, alarmNote }) => {
-        let alarmHTML = `<div class="alarm" data-alarm-id=${alarmId}>
-          <div class="leftContent">
-            <div class="leftTop">
-              <h1>${alarmTime}</h1>
-              <h5>${alarmExt}</h5>
-            </div>
-            <div class="leftBottom">
-              <p>Ring once</p>
-              ${alarmNote !== "" ? `<p>| ${alarmNote}</p>` : ""}
-            </div>
-          </div>
-          <input type="checkbox" name="onOffData" id="onOffData${alarmId}" ${
-          alarmStatus ? "checked" : ""
-        } />
-          <label for="onOffData${alarmId}"
-            ><div class="rightContent">
-              <div class="button ${alarmStatus ? "buttonActive" : ""}">
-                <div class="buttonDot ${alarmStatus ? "dotActive" : ""}"></div>
-              </div>
-            </div>
-          </label>
-        </div>`;
-        alarmContainer.insertAdjacentHTML("beforeend", alarmHTML);
+    displayAlarms(alarmContainer, alarmsData);
+  }
+});
+
+// Add Alarm
+let doneBtn = document.querySelector(".eventDone");
+doneBtn.addEventListener("click", () => {
+  pausePrevAudio();
+  ringtoneListContainer.classList.remove("showSongs");
+  let existingAlarms = JSON.parse(localStorage.getItem("alarms"));
+
+  if (existingAlarms === null || existingAlarms.length == 0) {
+    // Selected Ringtone
+    let ringtoneName;
+    let ringtonePath;
+    ringtones.forEach((ringtone) => {
+      if (ringtone.checked) {
+        ringtoneName = ringtone.getAttribute("data-song-name");
+        ringtonePath = ringtone.value;
       }
-    );
+    });
+    let alarmTime = timeInput.value;
+    let hour = alarmTime.split(":")[0];
+    let mins = alarmTime.split(":")[1];
+    let alarmExt = hour < 12 ? "AM" : "PM";
+    let curTime = Date.now();
+    let alarmYear = new Date().getFullYear();
+    let alarmMonth = new Date().getMonth();
+    let alarmDate = new Date().getDate();
+    let reqTime = new Date(
+      alarmYear,
+      alarmMonth,
+      alarmDate,
+      hour,
+      mins
+    ).getTime();
+    let alarmSetTime;
+    if (reqTime > curTime) {
+      alarmSetTime = reqTime;
+    } else {
+      alarmSetTime = reqTime + 1000 * 60 * 60 * 24;
+    }
+    let alarmNote = alarmNoteInput.value.trim();
+    let alarmStatus = true;
 
-    // Alarm On/Off Operation
-    let buttons = document.querySelectorAll(".button");
-    let buttonDots = document.querySelectorAll(".buttonDot");
-    let checkBoxes = document.querySelectorAll("input[type='checkbox']");
+    // Add New Alarm To Local Storage
+    let finalAlarmData = {
+      alarmId: 1,
+      alarmTime,
+      alarmExt,
+      alarmSetTime,
+      alarmNote,
+      alarmStatus,
+      ringtoneName,
+      ringtonePath,
+    };
+    localStorage.setItem("alarms", JSON.stringify([finalAlarmData]));
+  } else {
+    let editAlarmId = document
+      .querySelector(".alarmEventContainer")
+      .getAttribute("data-edit-alarm-id")
+      .trim();
 
-    buttons.forEach((val, ind) => {
-      val.addEventListener("click", () => {
-        alarmEventContainer.classList.remove("showAlarmEventContainer");
-        buttons[ind].classList.toggle("buttonActive");
-        buttonDots[ind].classList.toggle("dotActive");
-        let buttonStatus = !checkBoxes[ind].checked;
-        console.log(buttonStatus);
-      });
+    let alarmExistance = existingAlarms.find((alarm) => {
+      return alarm.alarmId == editAlarmId;
     });
 
-    // Update Alarm Info
-    let allAlarms = document.querySelectorAll(".alarm");
-    allAlarms.forEach((alarm, ind) => {
-      alarm.addEventListener("click", (e) => {
-        if (e.target.classList.contains("buttonDot")) {
-          console.log(e.target.classList);
-        } else {
-          timeInput.value = alarms[ind].alarmTime;
-          alarmNoteInput.value = alarms[ind].alarmNote;
-          selectedSong.textContent = alarms[ind].ringtoneName;
-          alarms[ind].alarmNote !== ""
-            ? resetBtn.classList.add("resetShow")
-            : resetBtn.classList.remove("resetShow");
-          ringtones.forEach((ringtone) => {
-            if (
-              ringtone.getAttribute("data-song-name").toLowerCase() ===
-              alarms[ind].ringtoneName.toLowerCase()
-            ) {
-              ringtone.checked = true;
-            }
-          });
-          alarmEventContainer.classList.add("showAlarmEventContainer");
+    if (alarmExistance !== undefined) {
+      let ringtoneName;
+      let ringtonePath;
+      ringtones.forEach((ringtone) => {
+        if (ringtone.checked) {
+          ringtoneName = ringtone.getAttribute("data-song-name");
+          ringtonePath = ringtone.value;
         }
       });
-    });
+      let alarmTime = timeInput.value;
+      let hour = alarmTime.split(":")[0];
+      let mins = alarmTime.split(":")[1];
+      let alarmExt = hour < 12 ? "AM" : "PM";
+      let curTime = Date.now();
+      let alarmYear = new Date().getFullYear();
+      let alarmMonth = new Date().getMonth();
+      let alarmDate = new Date().getDate();
+      let reqTime = new Date(
+        alarmYear,
+        alarmMonth,
+        alarmDate,
+        hour,
+        mins
+      ).getTime();
+      let alarmSetTime;
+      if (reqTime > curTime) {
+        alarmSetTime = reqTime;
+      } else {
+        alarmSetTime = reqTime + 1000 * 60 * 60 * 24;
+      }
+      let alarmNote = alarmNoteInput.value.trim();
+      let alarmStatus = existingAlarms.find((alarm) => {
+        return alarm.alarmId == editAlarmId;
+      }).alarmStatus;
+
+      // Add New Alarm To Local Storage
+      let finalAlarmData = {
+        alarmId: Number(editAlarmId),
+        alarmTime,
+        alarmExt,
+        alarmSetTime,
+        alarmNote,
+        alarmStatus,
+        ringtoneName,
+        ringtonePath,
+      };
+      let updatedAlarmData = [];
+      existingAlarms.forEach((alarm) => {
+        if (alarm.alarmId != editAlarmId) {
+          updatedAlarmData.push(alarm);
+        } else {
+          updatedAlarmData.push(finalAlarmData);
+        }
+      });
+
+      localStorage.setItem("alarms", JSON.stringify(updatedAlarmData));
+      alarmEventContainer.setAttribute("data-edit-alarm-id", "");
+    } else {
+      let ringtoneName;
+      let ringtonePath;
+      ringtones.forEach((ringtone) => {
+        if (ringtone.checked) {
+          ringtoneName = ringtone.getAttribute("data-song-name");
+          ringtonePath = ringtone.value;
+        }
+      });
+      let alarmTime = timeInput.value;
+      let hour = alarmTime.split(":")[0];
+      let mins = alarmTime.split(":")[1];
+      let alarmExt = hour < 12 ? "AM" : "PM";
+      let curTime = Date.now();
+      let alarmYear = new Date().getFullYear();
+      let alarmMonth = new Date().getMonth();
+      let alarmDate = new Date().getDate();
+      let reqTime = new Date(
+        alarmYear,
+        alarmMonth,
+        alarmDate,
+        hour,
+        mins
+      ).getTime();
+      let alarmSetTime;
+      if (reqTime > curTime) {
+        alarmSetTime = reqTime;
+      } else {
+        alarmSetTime = reqTime + 1000 * 60 * 60 * 24;
+      }
+      let alarmNote = alarmNoteInput.value.trim();
+      let alarmStatus = true;
+      let finalAlarmData = {
+        alarmId: existingAlarms[existingAlarms.length - 1].alarmId + 1,
+        alarmTime,
+        alarmExt,
+        alarmSetTime,
+        alarmNote,
+        alarmStatus,
+        ringtoneName,
+        ringtonePath,
+      };
+      let allAlarms = [...existingAlarms, finalAlarmData];
+      localStorage.setItem("alarms", JSON.stringify(allAlarms));
+    }
   }
+  alarmEventContainer.classList.remove("showAlarmEventContainer");
+  alarmNoteInput.value = "";
+  ringtones[0].checked = true;
+  alarmActive();
+  displayAlarms(alarmContainer, localStorage.getItem("alarms"));
 });
